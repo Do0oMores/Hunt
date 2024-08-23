4e61a49914a10b2df007846e26ec7a38a1951327
Do0oMores <do0omores@outlook.com>
2024-08-23 21:20:44 +0800

    FIX  更改为直接调用

--- src/main/java/top/mores/hunt/Animals/Level.java
+++ src/main/java/top/mores/hunt/Animals/Level.java
@@ -8,16 +8,51 @@ import java.util.List;
 public class Level {
     FileConfiguration config = Hunt.getInstance().config;
 
-    public List<String> NORMAL_LIST = config.getStringList("动物级别.普通.生物ID");
-    public int NORMAL_VALUE = config.getInt("动物级别.普通.价值");
-    public List<String> RARE_LIST = config.getStringList("动物级别.珍稀.生物ID");
-    public int RARE_VALUE = config.getInt("动物级别.珍稀.价值");
-    public List<String> SCARCE_LIST = config.getStringList("动物级别.少见.生物ID");
-    public int SCARCE_VALUE = config.getInt("动物级别.少见.价值");
-    public List<String> EPIC_LIST = config.getStringList("动物级别.史诗.生物ID");
-    public int EPIC_VALUE = config.getInt("动物级别.史诗.价值");
-    public List<String> LEGEND_LIST = config.getStringList("动物级别.传奇.生物ID");
-    public int LEGEND_VALUE = config.getInt("动物级别.传奇.价值");
-    public List<String> MYTH_LIST = config.getStringList("动物级别.神话.生物ID");
-    public int MYTH_VALUE = config.getInt("动物级别.神话.价值");
+    public List<String> NORMAL_LIST() {
+        return config.getStringList("动物级别.普通.生物ID");
+    }
+
+    public int NORMAL_VALUE() {
+        return config.getInt("动物级别.普通.价值");
+    }
+
+    public List<String> RARE_LIST() {
+        return config.getStringList("动物级别.珍稀.生物ID");
+    }
+
+    public int RARE_VALUE() {
+        return config.getInt("动物级别.珍稀.价值");
+    }
+
+    public List<String> SCARCE_LIST() {
+        return config.getStringList("动物级别.少见.生物ID");
+    }
+
+    public int SCARCE_VALUE() {
+        return config.getInt("动物级别.少见.价值");
+    }
+
+    public List<String> EPIC_LIST() {
+        return config.getStringList("动物级别.史诗.生物ID");
+    }
+
+    public int EPIC_VALUE() {
+        return config.getInt("动物级别.史诗.价值");
+    }
+
+    public List<String> LEGEND_LIST() {
+        return config.getStringList("动物级别.传奇.生物ID");
+    }
+
+    public int LEGEND_VALUE() {
+        return config.getInt("动物级别.传奇.价值");
+    }
+
+    public List<String> MYTH_LIST() {
+        return config.getStringList("动物级别.神话.生物ID");
+    }
+
+    public int MYTH_VALUE() {
+        return config.getInt("动物级别.神话.价值");
+    }
 }
