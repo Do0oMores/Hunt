dc14b92cb794f07ab537ce909a9f98d21259d450
Do0oMores <do0omores@outlook.com>
2024-08-23 11:44:34 +0800

    RENEW 配置文件和数据

--- src/main/java/top/mores/hunt/Hunt.java
+++ src/main/java/top/mores/hunt/Hunt.java
@@ -1,17 +1,88 @@
 package top.mores.hunt;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
 
 public final class Hunt extends JavaPlugin {
+    private static Hunt instance;
+    public FileConfiguration config;
+    public FileConfiguration data;
+    private File configFile;
+    private File dataFile;
+
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        initFiles();
+        getLogger().info("Hunt enabled");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Hunt disabled");
+    }
+
+    public Hunt getInstance() {
+        return instance;
+    }
+
+    public void reloadConfig() {
+        config = YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    public void reloadData() {
+        data = YamlConfiguration.loadConfiguration(dataFile);
+    }
+
+    private void initFiles() {
+        configFile = new File(getDataFolder(), "config.yml");
+        if (!configFile.exists()) {
+            boolean isCreateDir = configFile.getParentFile().mkdir();
+            if (!isCreateDir) {
+                getLogger().warning("mkdir config.yml error");
+                return;
+            }
+            saveResource("config.yml", false);
+        }
+        reloadConfig();
+
+        dataFile = new File(getDataFolder(), "data.yml");
+        if (!dataFile.exists()) {
+            try {
+                dataFile.createNewFile();
+            } catch (IOException e) {
+                getLogger().warning("mkdir data.yml error");
+            }
+        }
+        reloadData();
+    }
+
+    public FileConfiguration getData() {
+        if (data == null) {
+            reloadData();
+        }
+        return data;
+    }
+
+    public void saveData() {
+        try {
+            data.save(dataFile);
+        } catch (IOException e) {
+            getLogger().info("save data.yml error");
+        }
+    }
+
+    @Override
+    public @NotNull FileConfiguration getConfig(){
+        if (config==null){
+            reloadConfig();
+        }
+        return config;
     }
 }
